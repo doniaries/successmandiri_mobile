affac5e384f3bec843efbc4a08d47ba12130ea12
Doni Aries <doniaries@gmail.com>
2026-05-17 18:46:26 +0700

    fix(config): memperbarui script bat menggunakan method .Replace untuk menghindari encoding ampersand

--- konek_chrome.bat
+++ konek_chrome.bat
@@ -7,7 +7,7 @@ echo.
 echo Sedang mengkonfigurasi file API ke http://localhost:8000/api ...
 
 :: Overwrite/update api_constants.dart using PowerShell
-powershell -Command "$content = Get-Content 'lib/core/constants/api_constants.dart' -Raw; $content = $content -replace 'return ''http://127.0.0.1:8000/api''; // Infinix.*', 'return ''http://localhost:8000/api''; // Chrome' -replace '// return ''http://localhost:8000/api''; // Chrome', '// return ''http://127.0.0.1:8000/api''; // Infinix (via ADB Reverse) \u0026 Chrome'; Set-Content 'lib/core/constants/api_constants.dart' $content -NoNewline"
+powershell -Command "$content = Get-Content 'lib/core/constants/api_constants.dart' -Raw; $content = $content.Replace('return ''http://127.0.0.1:8000/api''; // Infinix (via ADB Reverse) & Chrome', 'return ''http://localhost:8000/api''; // Chrome').Replace('// return ''http://localhost:8000/api''; // Chrome', '// return ''http://127.0.0.1:8000/api''; // Infinix (via ADB Reverse) & Chrome'); Set-Content 'lib/core/constants/api_constants.dart' $content -NoNewline"
 
 echo.
 echo =============================================================
