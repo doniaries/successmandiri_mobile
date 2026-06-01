e5aaa367122375f2b78c2c0b3d9467923f54a876
Doni Aries <doniaries@gmail.com>
2026-06-02 01:43:10 +0700

    remove bat

--- test.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-set /p BUILD_NAME="1. Masukkan Versi Aplikasi (contoh: 1.0.5): "
-for /f "tokens=1,2,3 delims=." %%a in ("%BUILD_NAME%") do (
-    set BUILD_NUMBER=%%c
-)
-echo BUILD_NUMBER IS %BUILD_NUMBER%
--- test_version.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-set BUILD_NAME=1.0.2
-set BUILD_NUMBER=2
-powershell -Command "(Get-Content pubspec.yaml) -replace '^version:.*', 'version: %BUILD_NAME%+%BUILD_NUMBER%' | Set-Content pubspec.yaml"
-findstr /b "version:" pubspec.yaml
